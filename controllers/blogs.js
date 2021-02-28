25b989a5ba9f949e348c19f44cda7b34418e4dfa
Neil Ryan <neilryanlipaod@gmail.com>
2021-02-28 20:53:59 +0800

    Exercise 4.13 Blog list expansions, step1

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,11 +2,10 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const logger = require('../utils/logger');
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    logger.info(`---BLOGS---`, blogs);
-    response.json(blogs);
-  });
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  logger.info('--BLOGS--', blogs);
+  response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response, next) => {
@@ -17,6 +16,12 @@ blogsRouter.post('/', async (request, response, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  const { id } = request.params;
+
+  Blog.findByIdAndRemove(id).then(() => response.status(204).end());
 });
 
 module.exports = blogsRouter;
